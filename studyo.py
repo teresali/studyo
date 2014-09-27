50a2eac676ce786b852edb0f22604ef7b5c4abfe
Teresa Li <teresali@rrdhcp-10-32-52-101.redrover.cornell.edu>
2014-09-27 11:25:12 -0400

    added templates in

--- studyo.py
+++ studyo.py
@@ -1,9 +1,33 @@
+import os
+import urllib
+
+from google.appengine.api import users
+from google.appengine.ext import ndb
+
+import jinja2
 import webapp2
 
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+# [END imports]
+
+
+# [START main_page]
 class MainPage(webapp2.RequestHandler):
+
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, Teresa!')
+        template_values = {
+            'id': 1,
+            'name': "Teresa"
+        }
+
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(template_values))
+# [END main_page]
+
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
